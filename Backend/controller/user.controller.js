bdfcd3b613307387ac0c112a244558ea25b15090
arpit-yadav088 <ay715259@gmail.com>
2026-02-10 13:20:43 +0530

    Backend Jwt token task completed

--- Backend/controller/user.controller.js
+++ Backend/controller/user.controller.js
@@ -1,23 +1,25 @@
-
+import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
 import createTokenAndSaveCookie from "../jwt/generateToken.js"
+
 
 export const signup =async (req, res) => {
  try {
    const {name, email, password, confirmpassword} = req.body;
-  if(password !== confirmpassword) {
+    if(password !== confirmpassword) {
       return res.status(400).json({ Message: "Passwords do not match"});
     }
 
     const user = await User.findOne({ email});
- if (user) {
-  return res.status(400).json({ message: "email already exists" });
+    if (user) {
+     return res.status(400).json({ message: "email already exists" });
 }
-
+     //hasedPassword
+    const hashedPassword = await bcrypt.hash(password, 10);
     const newUser =await new User({
       name,
       email,
-      password,
+      password: hashedPassword,
     });
     await newUser.save();
     if (newUser){
@@ -32,3 +34,83 @@ export const signup =async (req, res) => {
   });
  }
 };
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+     if (!email || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+    const user = await User.findOne({ email });
+
+     // user check first
+   if (!user) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+    // password match
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "Invalid email or password" });
+    }
+
+       // token
+    createTokenAndSaveCookie(user._id, res);
+
+    // response
+    res.status(200).json({
+      message: "user logged in successfully",
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+      },
+    });
+
+  } catch (error) {
+     console.log("Login Error:", error.message);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+export const logout = async(req, res) => {
+   try {
+    res.clearCookie("jwt")
+    res.status(200).json({message: "User logged out successfully"});
+   } catch (error) {
+    console.log(error);
+    res.status(500)({message: "server error"});
+   }
+}
+
+// export const login = async (req, res) => {
+//   try {
+//     const { email, password } = req.body;
+
+//     const user = await User.findOne({ email });
+//     if (!user) {
+//       return res.status(401).json({ message: "Invalid email or password" });
+//     }
+
+//     const isMatch = await bcrypt.compare(password, user.password);
+//     if (!isMatch) {
+//       return res.status(401).json({ message: "Invalid email or password" });
+//     }
+
+//     createTokenAndSaveCookie(user._id, res);
+
+//     res.status(200).json({
+//       message: "User logged in successfully",
+//       user: {
+//         _id: user._id,
+//         name: user.name,
+//         email: user.email,
+//       },
+//     });
+
+//   } catch (error) {
+//     console.log("Login Error:", error.message);
+//     res.status(500).json({ message: "Server error" });
+//   }
+// };
--- Frontend/src/home/left/User.jsx
+++ Frontend/src/home/left/User.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const User = () => {
   return (
     <div>
-      <div className='flex space-x-4 py-7 hover:bg-slate-600 duration-300 cursor-pointer'>
+      <div className='flex space-x-4 py-7 hover:bg-slate-600 duration-300 cursor-pointer '>
       <div className="avatar avatar-online">
         <div className="w-12 rounded-full">
          <img src="https://img.daisyui.com/images/profile/demo/gordon@192.webp" />
