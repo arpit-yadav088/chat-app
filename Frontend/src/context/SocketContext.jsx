780f1fdd6646d90b9047b6738c3a98bdca3fa713
arpit-yadav088 <ay715259@gmail.com>
2026-04-24 18:51:46 +0530

    Final App Complete

--- Frontend/src/context/SocketContext.jsx
+++ Frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser?._id) {
-      const newSocket = io("http://localhost:5002", {
+      const newSocket = io("https://chat-app-x7zx.onrender.com", {
         query: {
           userId: authUser._id,
         },
